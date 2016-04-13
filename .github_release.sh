a166fbf6a105d3139c3807814f6ea0c8f5949604
Kei Okada <kei.okada@gmail.com>
2016-04-14 01:57:59 +0900

    show mesage every 5 min

--- .github_release.sh
+++ .github_release.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 set -e
-#set -x
 
 git clone -b v2.2.2 https://github.com/github/hub.git;
 (cd hub; ./script/build; sudo cp hub /usr/local/bin/)
@@ -10,7 +9,12 @@ mkdir -p ~/.config/
 echo "github.com:" > ~/.config/hub
 echo "- user: k-okada" >> ~/.config/hub
 echo "  oauth_token: $GITHUB_ACCESS_TOKEN" >> ~/.config/hub
-for isf in $CIRCLE_ARTIFACTS/*.iso ; do 
-    hub release create -p -a $iso -m "$CIRCLE_TAG"$'\n'"Released on `date '+%Y/%m/%d %H:%M:%S'`" $CIRCLE_TAG;
+
+set -x
+csplit CHANGELOG.rst '/^------------/' '{*}'
+for iso in $CIRCLE_ARTIFACTS/*.iso ; do
+    filename=`basename $iso .iso`-${CIRCLE_TAG//./_}.iso
+    aws s3 cp $iso s3://live-cd2/$filename  --acl public-read
+    hub release create -p -m "$CIRCLE_TAG"$'\n'"[Custom LIVE-CD $filename](https://s3-ap-northeast-1.amazonaws.com/live-cd2/$filename)"$'\n\n'"`sed '$d' xx01`"$'\n\n'"Released on `date '+%Y/%m/%d %H:%M:%S'`" $CIRCLE_TAG
 done
 
