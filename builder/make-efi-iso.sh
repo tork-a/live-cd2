16fd164338ae1dfb616eda0524a3bbfec8539401
Kei Okada <kei.okada@gmail.com>
2016-06-01 12:02:15 +0900

    update grub.cfg menu for UEFI boot

--- builder/make-efi-iso.sh
+++ builder/make-efi-iso.sh
@@ -22,6 +22,8 @@ sudo sed -i 's@^ui @#ui @' isolinux/isolinux.cfg
 # remove prompt to eject cd : http://www.pendrivelinux.com/ubuntu-remove-the-prompt-to-eject-cd/
 sudo sed -i 's@quiet splash --@quiet splash noprompt username=tork --@' isolinux/txt.cfg
 sudo sed -i 's@Ubuntu@ROS Ubuntu $(git describe --abbrev=0 --tags)@' isolinux/txt.cfg
+sudo sed -i 's@quiet splash --@quiet splash noprompt username=tork --@' boot/grub/grub.cfg
+sudo sed -i 's@Ubuntu@ROS Ubuntu $(git describe --abbrev=0 --tags)@' boot/grub/grub.cfg
 
 cd ..
 cat <<EOF | sudo tee sort.txt
