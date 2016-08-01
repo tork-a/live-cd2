8b39c1c0014b31eedc32a94bb55bf17dc090b05a
Kei Okada <kei.okada@gmail.com>
2016-08-01 23:10:50 +0900

    ./github_release.sh : sleep 60 after aws s3 cp

--- .github_release.sh
+++ .github_release.sh
@@ -15,6 +15,8 @@ csplit CHANGELOG.rst '/^------------/' '{*}'
 for iso in $CIRCLE_ARTIFACTS/*.iso ; do
     filename=`basename $iso .iso`-${CIRCLE_TAG//./_}.iso
     aws s3 cp $iso s3://live-cd2/$filename  --acl public-read
+    hub releaes create -p $CIRCLE_TAG ; echo "ok"; sleep 10;
+    hub releaes
     hub release create -p -m "$CIRCLE_TAG"$'\n'"[Custom LIVE-CD $filename](https://s3-ap-northeast-1.amazonaws.com/live-cd2/$filename)"$'\n\n'"`sed '$d' xx01`"$'\n\n'"Released on `date '+%Y/%m/%d %H:%M:%S'`" $CIRCLE_TAG
 done
 
